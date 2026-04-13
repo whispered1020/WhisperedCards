69b7295a528a575a402da3726b357c42cd8bbaf0
Vinny Reis <38574940+whispered1020@users.noreply.github.com>
2026-04-13 10:22:26 -0300

    Updated Biome Field Spell

--- script/c2009000023.lua
+++ script/c2009000023.lua
@@ -10,7 +10,7 @@ function s.initial_effect(c)
 	e1:SetTarget(s.thtg)
 	e1:SetOperation(s.thop)
 	c:RegisterEffect(e1)
-    -- Ritual Summon 1 "Biome" Ritual Monster
+    --Ritual Summon 1 "Biome" Ritual Monster
 	local e2=Ritual.CreateProc(c,RITPROC_GREATER,aux.FilterBoolFunction(Card.IsSetCard,0xf19),nil,aux.Stringid(id,1))
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetCountLimit(2,{id,1})
@@ -21,16 +21,24 @@ s.listed_series={0xf19}
 function s.thfilter(c)
 	return c:IsSetCard(0xf19) and c:IsMonster() and c:IsAbleToHand()
 end
+function s.thritualfilter(c)
+    return c:IsSetCard(0xf19) and c:IsType(TYPE_RITUAL) and c:IsMonster() and c:IsAbleToHand()
+end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,2,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) and 
+		Duel.IsExistingMatchingCard(s.thritualfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,2,tp,LOCATION_DECK)
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,2,tp,LOCATION_HAND)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,2,2,nil)
-	if #g>0 and Duel.SendtoHand(g,nil,REASON_EFFECT)>0 and g:GetFirst():IsLocation(LOCATION_HAND) then
-		Duel.ConfirmCards(1-tp,g)
+	local rg=Duel.SelectMatchingCard(tp,s.thritualfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #rg==0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,rg:GetFirst())
+	rg:Merge(g)
+	if #rg>0 and Duel.SendtoHand(rg,nil,REASON_EFFECT)>0 then
+		Duel.ConfirmCards(1-tp,rg)
 		Duel.ShuffleHand(tp)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
 		local dg=Duel.SelectMatchingCard(tp,Card.IsAbleToDeck,tp,LOCATION_HAND,0,2,2,nil)
