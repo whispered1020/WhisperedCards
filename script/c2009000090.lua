bcc63e05fdae917da05dc3a73e20bcc4ddee31c7
Vinny Reis <38574940+whispered1020@users.noreply.github.com>
2026-04-25 16:57:26 -0300

    Updated Harsh Desert

--- script/c2009000090.lua
+++ script/c2009000090.lua
@@ -24,26 +24,11 @@ function s.condition(e,tp,eg,ep,ev,re,r,rp)
   return tp~=ep and Duel.GetCurrentChain()==0
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-  if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
   if chk==0 then return true end
   Duel.SetOperationInfo(0,CATEGORY_DISABLE_SUMMON,eg,#eg,0,0)
   Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,#eg,0,0)
-  if #eg==1 and Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsDestructable),tp,0,LOCATION_MZONE,1,nil) then
-    Duel.SetPossibleOperationInfo(0,CATEGORY_DESTROY,nil,1,1-tp,LOCATION_MZONE)
-  end
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-  local ct=#eg
-  if Duel.NegateSummon(eg) then
-    Duel.Destroy(eg,REASON_EFFECT)
-    if ct==1 then
-      local g=Duel.GetMatchingGroup(s.desfilter,tp,0,LOCATION_MZONE,nil)
-      if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-        local sg=g:Select(tp,1,1,nil)
-        Duel.HintSelection(sg)
-        Duel.Destroy(sg,REASON_EFFECT)
-      end
-    end
-  end
+  Duel.NegateSummon(eg)
+  Duel.Destroy(eg,REASON_EFFECT)
 end
