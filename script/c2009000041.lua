0f01a509c5852ea57908464dabdae8a2b970a565
Vinny Reis <38574940+whispered1020@users.noreply.github.com>
2026-05-01 20:22:05 -0300

    Updated Predaplant Umbracalyx

--- script/c2009000041.lua
+++ script/c2009000041.lua
@@ -12,17 +12,17 @@ function s.initial_effect(c)
 	e1:SetTargetRange(0,LOCATION_MZONE)
 	e1:SetValue(s.atkval)
 	c:RegisterEffect(e1)
-	--Banish a monster your opponent controls with Predator Counter that activates its effect
+	--Inflict damage
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetCategory(CATEGORY_REMOVE)
-	e2:SetType(EFFECT_TYPE_QUICK_F)
-	e2:SetCode(EVENT_CHAINING)
-	e2:SetRange(LOCATION_MZONE)
+	e2:SetCategory(CATEGORY_DAMAGE)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetCode(EVENT_BATTLE_DESTROYING)
 	e2:SetCountLimit(1,id)
-	e2:SetCondition(s.rmcon)
-	e2:SetTarget(s.rmtg)
-	e2:SetOperation(s.rmop)
+	e2:SetCondition(aux.bdcon)
+	e2:SetTarget(s.damtg)
+	e2:SetOperation(s.damop)
 	c:RegisterEffect(e2)
 end
 
@@ -32,23 +32,19 @@ function s.atkval(e,c)
 	if c:GetCounter(COUNTER_PREDATOR)>0 then
 		return -minus
 	else
-		return -700
+		return -500
 	end
 end
---banish
-function s.rmcon(e,tp,eg,ep,ev,re,r,rp)
-    local rc=re:GetHandler()
-    return ep==1-tp and rc:IsOnField() and rc:IsControler(1-tp)
-        and rc:GetCounter(COUNTER_PREDATOR)>0
+--Inflict damage
+function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local dam=e:GetHandler():GetBattleTarget():GetAttack()
+	if dam<0 then dam=0 end
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(dam)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,dam)
 end
-function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    local rc=re:GetHandler()
-    if chk==0 then return rc:IsAbleToRemove() end
-    Duel.SetOperationInfo(0,CATEGORY_REMOVE,rc,1,0,0)
-end
-function s.rmop(e,tp,eg,ep,ev,re,r,rp)
-    local rc=re:GetHandler()
-    if rc:IsRelateToEffect(re) and rc:IsOnField() and rc:GetCounter(COUNTER_PREDATOR)>0 then
-        Duel.Remove(rc,POS_FACEUP,REASON_EFFECT)
-    end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Damage(p,d,REASON_EFFECT)
 end
