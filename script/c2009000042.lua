4e14053526aa03225a2be04c2fb62d0802413690
Vinny Reis <38574940+whispered1020@users.noreply.github.com>
2026-05-03 13:43:46 -0300

    Updated Predaplant Catopsphinx

--- script/c2009000042.lua
+++ script/c2009000042.lua
@@ -1,43 +1,111 @@
 --Predaplant Catopsphinx
+--Scripted by: Whispered
 local s,id=GetID()
 function s.initial_effect(c)
+	Pendulum.AddProcedure(c)
 	--Fusion materials
 	c:EnableReviveLimit()
 	Fusion.AddProcFunRep(c,aux.FilterBoolFunctionEx(Card.IsSetCard,SET_PREDAPLANT),2,true)
-	--Quick Fusion Summon during opponent's Main Phase
-	local params={aux.FilterBoolFunction(Card.IsRace,RACE_PLANT),s.matfilter,s.fextra,Fusion.BanishMaterial,nil}
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
-	e1:SetType(EFFECT_TYPE_QUICK_O)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E+TIMING_MAIN_END)
-	e1:SetCountLimit(1,id)
-	e1:SetCondition(s.fuscon)
-	e1:SetCost(s.fuscost)
-	e1:SetTarget(Fusion.SummonEffTG(table.unpack(params)))
-	e1:SetOperation(Fusion.SummonEffOP(table.unpack(params)))
-	c:RegisterEffect(e1)
+	--Fusion Effect
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetRange(LOCATION_PZONE)
+    e1:SetCountLimit(1,id)
+    e1:SetTarget(s.fustg)
+    e1:SetOperation(s.fusop)
+    c:RegisterEffect(e1)
+    --Search 1 "Predaplant" Pendulum Monster
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(function(e) return e:GetHandler():IsFusionSummoned() end)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
+	--place itself in pendulum zone
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_TO_GRAVE)
+	e3:SetCountLimit(1,{id,2})
+	e3:SetCondition(s.pzcon)
+	e3:SetTarget(function(e,tp,eg,ep,ev,re,r,rp,chk) if chk==0 then return Duel.CheckPendulumZones(tp) end local c=e:GetHandler() if c:IsLocation(LOCATION_GRAVE) then Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,c,1,tp,0) end end)
+	e3:SetOperation(s.pzop)
+	c:RegisterEffect(e3)
+	local e4=e3:Clone()
+	e4:SetCode(EVENT_TO_DECK)
+	e4:SetCondition(s.pz2con)
+	c:RegisterEffect(e4)
 end
 s.material_setcode={0x10f3}
 s.counter_place_list={COUNTER_PREDATOR}
 
-function s.fuscon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()~=tp and Duel.IsMainPhase()
-end
-function s.fuscost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsReleasable() end
-	Duel.Release(e:GetHandler(),REASON_COST)
-end
-function s.opppred(c)
-	return c:IsFaceup() and c:IsMonster() and c:GetCounter(COUNTER_PREDATOR)>0
+function s.filter(c)
+	return c:IsFaceup() and c:IsSetCard(SET_PREDAPLANT) and c:IsType(TYPE_PENDULUM)
 end
 function s.fextra(e,tp,mg)
-	local gy=Duel.GetMatchingGroup(aux.NecroValleyFilter(Card.IsAbleToRemove),tp,LOCATION_GRAVE,0,nil)
-	local opp=Duel.GetMatchingGroup(Fusion.IsMonsterFilter(s.opppred),tp,0,LOCATION_MZONE,nil)
-	return gy+opp
+	return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(s.filter),tp,0,LOCATION_EXTRA,nil)
 end
-function s.matcheck(tp,sg,fc)
-	return sg:IsExists(Card.IsSetCard,1,nil,0x10f3)
+function s.fusfilter(c,e,tp)
+    return c:IsType(TYPE_FUSION) and c:IsAttribute(ATTRIBUTE_DARK)
+        and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)
+end
+function s.fustg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then
+        return Duel.IsExistingMatchingCard(s.fusfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp)
+    end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function s.fusop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if not c:IsRelateToEffect(e) then return end
+    -- Gather materials: this card + Pendulum Monsters in face-up Extra Deck
+    local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_EXTRA,0,nil)
+    g:AddCard(c)
+    -- Select DARK Fusion Monster
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local sg=Duel.SelectMatchingCard(tp,s.fusfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
+    local tc=sg:GetFirst()
+    if tc then
+        Duel.FusionSummon(tp,tc,nil,g)
+    end
+end
+--
+function s.thfilter(c)
+	return c:IsAbleToHand() and (c:IsFaceup() or not c:IsLocation(LOCATION_EXTRA))
+		and (c:IsSetCard(SET_PREDAPLANT) and c:IsMonster() and c:IsType(TYPE_PENDULUM)))
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK|LOCATION_EXTRA,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK|LOCATION_EXTRA)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK|LOCATION_EXTRA,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+--
+function s.pzcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsReason(REASON_EFFECT)
+end
+function s.pz2con(e)
+	local c=e:GetHandler()
+	return c:IsLocation(LOCATION_EXTRA) and c:IsFaceup()
+end
+function s.pzop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.MoveToField(c,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
+	end
 end
