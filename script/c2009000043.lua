724372d08b3a18ded05aae4b98a25a11808ea8e9
Vinny Reis <38574940+whispered1020@users.noreply.github.com>
2026-05-03 15:21:20 -0300

    Fixed Predaplant Umbracalyx

--- script/c2009000043.lua
+++ script/c2009000043.lua
@@ -49,7 +49,7 @@ end
 --Special Summon
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if not Duel.GetMZoneCount(tp)>0 then return false end 
+	if not Duel.GetMZoneCount(tp) then return false end 
 	if chk==0 then return Duel.GetMZoneCount>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,0)
 end
