d970f2bb8bec4c5678ada480f01460f7fd9d143a
Vinny Reis <38574940+whispered1020@users.noreply.github.com>
2026-05-15 15:51:58 -0300

    Updated Shirei

--- script/c2009000071.lua
+++ script/c2009000071.lua
@@ -5,88 +5,127 @@ function s.initial_effect(c)
 	c:EnableReviveLimit()
 	-- Synchro Summon procedure
 	Synchro.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_FIEND),1,1,Synchro.NonTuner(nil),1,1)
-    --banish
+    --Return 1 banished monster to Deck then banish 1 monster on field until the end phase
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_REMOVE)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e1:SetCode(EVENT_BATTLE_DESTROYED)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_REMOVE+CATEGORY_TODECK)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetTarget(s.btg)
-	e1:SetOperation(s.bop)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.bttg)
+	e1:SetOperation(s.btop)
 	c:RegisterEffect(e1)
-    --Gains 100 ATK/DEF for each banished card
+    --banish opponent's monster from GY
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_REMOVE)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_MOVE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCode(EFFECT_UPDATE_ATTACK)
-	e2:SetValue(s.atkval)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(s.bcon)
+	e2:SetTarget(s.btg)
+	e2:SetOperation(s.bop)
 	c:RegisterEffect(e2)
-	local e3=e2:Clone()
-	e3:SetCode(EFFECT_UPDATE_DEFENSE)
+    --Gains 100 ATK/DEF for each banished card
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCode(EFFECT_UPDATE_ATTACK)
+	e3:SetValue(s.atkval)
 	c:RegisterEffect(e3)
-	--Negate the effects of an opponent's monster
-	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,0))
-	e4:SetCategory(CATEGORY_DISABLE)
-	e4:SetType(EFFECT_TYPE_QUICK_O)
-	e4:SetCode(EVENT_FREE_CHAIN)
-	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e4:SetRange(LOCATION_MZONE)
-	e4:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
-	e4:SetCountLimit(1,id)
-	e4:SetTarget(s.distg)
-	e4:SetOperation(s.disop)
+	local e4=e3:Clone()
+	e4:SetCode(EFFECT_UPDATE_DEFENSE)
 	c:RegisterEffect(e4)
+	
 end
 
 function s.tunerfilter(c,scard,sumtype,tp)
 	return c:IsRace(RACE_FIEND) and c:IsAttribute(ATTRIBUTE_LIGHT)
 end
 --
-function s.bfilter(c,e,tp)
-	return c:IsLocation(LOCATION_GRAVE) and c:IsReason(REASON_BATTLE)
-		and c:IsCanBeEffectTarget(e) and c:IsAbleToRemove()
+function s.bfilter(c)
+	return c:IsSetCard(0x2045) and c:IsAbleToDeckAsCost()
+end
+function s.btfilter(c)
+	return c:IsMonster() and c:IsAbleToRemove()
+end
+function s.bttg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsOnField() and chkc:IsAbleToRemove() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD,1,nil) 
+		and Duel.IsExistingMatchingCard(s.bfilter,tp,LOCATION_REMOVED,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,s.bfilter,tp,LOCATION_REMOVED,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,0,0)
+end
+function s.btop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and Duel.SendtoDeck(tc,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)~=0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+		local g=Duel.SelectTarget(tp,s.btfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+		if g and Duel.Remove(g,0,REASON_EFFECT|REASON_TEMPORARY)~=0 then
+			g:RegisterFlagEffect(id,RESETS_STANDARD_PHASE_END,0,1)
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+			e1:SetCode(EVENT_PHASE+PHASE_END)
+			e1:SetReset(RESET_PHASE|PHASE_END)
+			e1:SetLabelObject(g)
+			e1:SetCountLimit(1)
+			e1:SetCondition(s.retcon)
+			e1:SetOperation(s.retop)
+			Duel.RegisterEffect(e1,tp)
+		end
+	end
+end
+function s.retcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetLabelObject():GetFlagEffect(id)~=0
+end
+function s.retop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.ReturnToField(e:GetLabelObject())
+end
+--
+function s.confilter(c,tp)
+	return c:IsPreviousLocation(LOCATION_REMOVED) and not c:IsPreviousControler(tp)
+end
+function s.bcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.confilter,1,nil,tp)
 end
 function s.btg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return eg:IsContains(chkc) and s.bfilter(chkc,e,tp) end
-	if chk==0 then return eg:IsExists(s.bfilter,1,nil,e,tp) end
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(1-tp) and chkc:IsAbleToRemove() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToRemove,tp,0,LOCATION_GRAVE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=eg:FilterSelect(tp,s.bfilter,1,1,nil,e,tp)
-	Duel.SetTargetCard(g)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToRemove,tp,0,LOCATION_GRAVE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
 end
 function s.bop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) then
-		Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
+	if tc:IsRelateToEffect(e) and Duel.Remove(tc,0,REASON_EFFECT|REASON_TEMPORARY)>0 then
+		tc:RegisterFlagEffect(id,RESETS_STANDARD_PHASE_END,0,1)
+		--Return the monster in the Main Phase 2
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_PHASE+PHASE_MAIN2)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_MAIN2)
+		e1:SetLabelObject(tc)
+		e1:SetCountLimit(1)
+		e1:SetCondition(s.retcon2)
+		e1:SetOperation(s.retop2)
+		Duel.RegisterEffect(e1,tp)
 	end
+end
+function s.retcon2(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetLabelObject():HasFlagEffect(id)
+end
+function s.retop2(e,tp,eg,ep,ev,re,r,rp)
+	Duel.SendtoGrave(e:GetLabelObject(),REASON_EFFECT)
 end
 --
 function s.atkval(e,c)
 	return Duel.GetFieldGroupCount(0,LOCATION_REMOVED,LOCATION_REMOVED)*100
-end
---
-function s.distg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and chkc:IsNegatableMonster() end
-	if chk==0 then return Duel.IsExistingTarget(Card.IsNegatableMonster,tp,0,LOCATION_MZONE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_NEGATE)
-	local g=Duel.SelectTarget(tp,Card.IsNegatableMonster,tp,0,LOCATION_MZONE,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,tp,0)
-end
-function s.disop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local tc=Duel.GetFirstTarget()
-	if not (tc:IsRelateToEffect(e) and tc:IsFaceup() and tc:IsCanBeDisabledByEffect(e)) then return end
-	local dc=Duel.TossDice(tp,1)
-	if dc==2 or dc==3 or dc==4 or dc==5 then
-		tc:NegateEffects(e:GetHandler(),RESETS_STANDARD_PHASE_END)
-		Duel.AdjustInstantly(tc)
-	end
-	if dc==6 then
-		if tc:NegateEffects(e:GetHandler(),RESETS_STANDARD_PHASE_END) and tc:IsRelateToEffect(re) then
-			Duel.Destroy(tc,REASON_EFFECT)
-		end
-	end
 end
