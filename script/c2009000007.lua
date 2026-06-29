beeaa2b48cbbd79d380be6d22b07a3f976191c5a
Vinny Reis <38574940+whispered1020@users.noreply.github.com>
2026-06-29 17:09:31 -0300

    Updated Queen Penguin

--- script/c2009000007.lua
+++ script/c2009000007.lua
@@ -15,7 +15,7 @@ function s.initial_effect(c)
 	e1:SetTarget(s.eqtg)
 	e1:SetOperation(s.eqop)
 	c:RegisterEffect(e1)
-		--position
+	--position
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_POSITION)
@@ -23,12 +23,12 @@ function s.initial_effect(c)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(2,id)
+	e2:SetCountLimit(2)
 	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
 	e2:SetTarget(s.settg)
 	e2:SetOperation(s.setop)
 	c:RegisterEffect(e2)
-		--indes
+	--indes
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD)
 	e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
@@ -37,6 +37,31 @@ function s.initial_effect(c)
 	e3:SetTarget(s.indtg)
 	e3:SetValue(1)
 	c:RegisterEffect(e3)
+	--Banish opponent card if a card is set
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,2))
+	e4:SetCategory(CATEGORY_REMOVE)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e4:SetCode(EVENT_CHANGE_POS)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1,id)
+	e4:SetCondition(s.bcon1)
+	e4:SetTarget(s.btg)
+	e4:SetOperation(s.bop)
+	c:RegisterEffect(e4)
+	local e5=e4:Clone()
+	e5:SetCode(EVENT_MSET)
+	e5:SetCondition(s.bcon2)
+	c:RegisterEffect(e5)
+	local e6=e4:Clone()
+	e6:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e6:SetCondition(s.bcon3)
+	c:RegisterEffect(e6)
+	local e7=e4:Clone()
+	e7:SetCode(EVENT_SSET)
+	e7:SetCondition(s.bcon4)
+	c:RegisterEffect(e7)
 end
 
 function s.filter(c,ec)
@@ -76,4 +101,42 @@ end
 --protection for Spell/Trap
 function s.indtg(e,c)
 	return c:GetSequence()<5 and c:IsFaceup()
+end
+--
+function s.filter1(c,tp)
+	return c:IsPreviousPosition(POS_FACEUP) and c:IsFacedown()
+end
+function s.bcon1(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.filter1,1,nil,tp)
+end
+function s.filter2(c,tp)
+	return c:IsFacedown()
+end
+function s.bcon2(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.filter2,1,nil,tp)
+end
+function s.filter3(c,tp)
+	return c:IsFacedown() and c:IsLocation(LOCATION_MZONE)
+end
+function s.bcon3(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.filter3,1,nil,tp)
+end
+function s.filter4(c,tp)
+	return c:IsFacedown() and c:IsLocation(LOCATION_SZONE)
+end
+function s.bcon4(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.filter4,1,nil,tp)
+end
+function s.btg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_ONFIELD) and chkc:IsControler(1-tp) and chkc:IsAbleToRemove() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
+end
+function s.bop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
+	end
 end
