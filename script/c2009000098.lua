f436857d494aeb1d3ceeb72dae5a48bc8dd99464
Vinny Reis <38574940+whispered1020@users.noreply.github.com>
2026-07-04 02:39:34 -0300

    Updated Hihiirokane

--- script/c2009000098.lua
+++ script/c2009000098.lua
@@ -17,17 +17,19 @@ function s.initial_effect(c)
     e1:SetTarget(s.pltg)
     e1:SetOperation(s.plop)
     c:RegisterEffect(e1)
-    --Fusion Summon (Quick Effect)
+    -- Fusion Effect
+    local params = {fusfilter=aux.FilterBoolFunction(Card.IsSetCard,SET_METALFOES),matfilter=aux.FALSE,extrafil=s.extramaterial}
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
-    e2:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
     e2:SetType(EFFECT_TYPE_QUICK_O)
+    e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
     e2:SetCode(EVENT_FREE_CHAIN)
     e2:SetRange(LOCATION_MZONE)
-    e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
     e2:SetCountLimit(1,{id,1})
-    e2:SetTarget(s.fustg)
-    e2:SetOperation(s.fusop)
+    e2:SetCost(s.fuscost)
+    e2:SetTarget(Fusion.SummonEffTG(params))
+    e2:SetOperation(Fusion.SummonEffOP(params))
     c:RegisterEffect(e2)
 end
 
@@ -49,34 +51,29 @@ function s.pltg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.IsExistingMatchingCard(s.plfilter,tp,LOCATION_DECK,0,1,nil) end
 end
 function s.plop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOEXTRA)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SELECT)
     local g=Duel.SelectMatchingCard(tp,s.plfilter,tp,LOCATION_DECK,0,1,1,nil)
     if #g>0 then
         Duel.SendtoExtraP(g,tp,REASON_EFFECT)
     end
 end
 --Fusion Summon
-function s.fusfilter(c,e,tp)
-    return c:IsSetCard(SET_METALFOES) and c:IsType(TYPE_FUSION) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)
+function s.costfilter(c)
+    return c:IsSetCard(SET_METALFOES) and c:IsDestructable()
 end
-function s.fustg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_ONFIELD,0,2,nil)
-        and Duel.IsExistingMatchingCard(Card.IsSetCard,tp,LOCATION_EXTRA+LOCATION_HAND,0,1,nil,SET_METALFOES)
-    end
+function s.fusfilter(c)
+    return c:IsMonster() and (c:IsLocation(LOCATION_HAND) or c:IsFaceup())
+end
+function s.extramaterial(e,tp,mg)
+    local g=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsAbleToGrave),tp,LOCATION_EXTRA,0,nil)
+    local g2=Duel.GetMatchingGroup(Card.IsAbleToGrave,tp,LOCATION_HAND,0,nil)
+    g:AddCard(g2)
+    return g
+end
+function s.fuscost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_ONFIELD,0,2,nil)
+        and Duel.IsExistingMatchingCard(s.fusfilter,tp,LOCATION_EXTRA|LOCATION_HAND,0,2,nil) end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-    local g=Duel.SelectTarget(tp,Card.IsSetCard,tp,LOCATION_ONFIELD,0,2,2,nil,SET_METALFOES)
-    Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,2,0,0)
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
-end
-function s.fusop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    local g=Duel.GetTargetCards(e)
-    if #g<2 or Duel.Destroy(g,REASON_EFFECT)<2 then return end
-    local mg=Duel.GetMatchingGroup(Card.IsCanBeFusionMaterial,tp,LOCATION_HAND+LOCATION_EXTRA,0,nil)
-    if #mg==0 then return end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-    local sg=Duel.SelectMatchingCard(tp,s.fusfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
-    if #sg>0 then
-        Duel.FusionSummon(tp,sg:GetFirst(),mg)
-    end
+    local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_ONFIELD,0,2,2,nil)
+    Duel.Destroy(g,REASON_COST)
 end
