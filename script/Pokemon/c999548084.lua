228388878f7ab03c31c98ec67d5c700bacf84e97
Vinny Reis <vinnysouzareis1@gmail.com>
2026-07-25 22:04:54 -0300

    Modified PlusPower

--- script/Pokemon/c999548084.lua
+++ script/Pokemon/c999548084.lua
@@ -3,17 +3,5 @@
 Duel.LoadScript("pokeutil.lua")
 local s,id=GetID()
 function s.initial_effect(c)
-	pokeutil.toolAttach(c,nil,nil,s.tgop,nil)
-end
-
---function to attach the tool only to the active pokemon, using function pokeutil.filterEx()
-function s.tgop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    if chkc then return chkc:IsLocation(LOCATION_MZONE) end
-	if chk==0 then return e:IsHasType(EFFECT_TYPE_ACTIVATE) and Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-			Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
-end
-function s.filter(c)
-    return pokeutil.toolfilt(c) and pokeutil.filterEx(c)
+	pokeutil.toolAttach(c,nil,nil,nil,nil)
 end
