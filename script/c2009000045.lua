32b59ea6ef1dc46f97eed07878218ab705c76a51
Vinny Reis <vinnysouzareis1@gmail.com>
2026-07-31 15:40:45 -0300

    Updated Revenil

--- script/c2009000045.lua
+++ script/c2009000045.lua
@@ -46,7 +46,7 @@ function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function s.drop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and Duel.SendtoDeck(tc,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)~=0 and tc:IsLocation(LOCATION_DECK+LOCATION_EXTRA) then
+	if tc:IsRelateToEffect(e) and Duel.SendtoDeck(tc,tp,SEQ_DECKSHUFFLE,REASON_EFFECT)>0 and tc:IsLocation(LOCATION_DECK+LOCATION_EXTRA) then
 		Duel.BreakEffect()
 		if Duel.Draw(tp,1,REASON_EFFECT)~=0 then
 			Duel.ShuffleHand(tp)
