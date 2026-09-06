e73b80bd77fab94e1ec3c10a16466fcdc9d637ec
Vinny Reis <vinnysouzareis1@gmail.com>
2026-09-06 14:31:36 -0300

    Fixed cycle of ages

--- script/c2009000142.lua
+++ script/c2009000142.lua
@@ -7,18 +7,18 @@ function s.initial_effect(c)
 	e0:SetType(EFFECT_TYPE_ACTIVATE)
 	e0:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e0)
-	--Draw 1, then discard 1
+	--cannot be destroyed by card effects
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_DRAW+CATEGORY_HANDES)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_CHAINING)
-	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
 	e1:SetRange(LOCATION_FZONE)
+	e1:SetTargetRange(LOCATION_SZONE,LOCATION_SZONE)
 	e1:SetCountLimit(1,id)
-	e1:SetCondition(s.drcon)
-	e1:SetTarget(s.drtg)
-	e1:SetOperation(s.drop)
+	e1:SetCondition(s.protcon)
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsPosition,POS_FACEDOWN))
+	e1:SetValue(1)
 	c:RegisterEffect(e1)
 	--Send 1 "Eonwheel" card from Deck to GY
 	local e2=Effect.CreateEffect(c)
@@ -35,34 +35,12 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 
-function s.drcon(e,tp,eg,ep,ev,re,r,rp)
-    local loc=Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_LOCATION)
-    return Duel.IsTurnPlayer(1-tp)
-		and re:IsHasType(EFFECT_TYPE_ACTIVATE)
-        and re:GetHandler():IsSetCard(0xf22)
-        and (loc&LOCATION_HAND)~=0
-        and rp==tp
-		
+function s.protfilter(c)
+	return c:IsSetCard(0xf22) and c:IsFaceup() and not c:IsCode(id)
 end
-function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
-	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
-end
-function s.drop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.Draw(tp,1,REASON_EFFECT)==0 then return end
-	local hg=Duel.GetFieldGroup(tp,LOCATION_HAND,0)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
-	local dg=hg:Select(tp,1,1,nil)
-	if #dg==0 then return end
-	Duel.SendtoGrave(dg,REASON_EFFECT+REASON_DISCARD)
-	local dc=dg:GetFirst()
-	if not dc or not dc:IsLocation(LOCATION_GRAVE) then return end
-	if not dc:IsSetCard(0xf22) then return end
-	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
-	if not dc:IsSSetable() then return end
-	if Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
-	    Duel.SSet(tp,dc)
-    end
+function s.protcon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.IsExistingMatchingCard(Card.IsSetCard,tp,LOCATION_GRAVE,0,1,nil,0xf22) or
+		Duel.IsExistingMatchingCard(s.protfilter,tp,LOCATION_MZONE,0,1,nil)
 end
 --
 function s.returnfilter(c,tp)
